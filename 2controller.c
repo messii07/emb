7303bee9a66871fb76a60960bbf07b01fed7a7d4
messii07 <146841557+messii07@users.noreply.github.com>
2024-03-18 06:44:38 +0530

    Update 2controller.c

--- 2controller.c
+++ 2controller.c
@@ -1,3 +1,4 @@
+//controller:1
 #include<reg51.h>
 void Delay(unsigned int time);
 void main(void)
@@ -15,4 +16,23 @@ void Delay(unsigned int time)
 		for (j = 0; j < 23; j++){
 		}
 	}
+}
+
+//controller 2
+#include<reg51.h>
+void Delay(unsigned int time);
+void main(void)
+{
+while(1){
+P2 = P1;
+Delay(1000);
+}
+}
+void Delay(unsigned int time)
+{
+unsigned int i,j;
+for (i = 0; i < time; i++){
+for (j = 0; j < 23; j++){
+}
+}
 }
